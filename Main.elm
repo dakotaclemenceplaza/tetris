fb43ff1acfbb57b12efb1bc35a5c6f22ec9b2d8a
A D <asdkffdsfj@gmail.com>
2017-10-12 13:17:42 +0400

    Refactor

--- Main.elm
+++ Main.elm
@@ -12,33 +12,33 @@ import Update exposing (..)
 main = program { init = init, update = update, subscriptions = subscriptions, view = view }
 
 type alias Model =
-    { figure : Maybe Figure,
+    { figure : Figure,
       rotation : Int,
-      rotationSize : Int,
       currentPos : Rotation,
       position : Position,
       pile : Pile,
       pause : Bool,
-      down : Bool
+      down : Bool,
+      start : Bool
     }
 
 model =
-    { figure = Just (fromList [(0, [])]),
+    { figure = fromList [(0, [])],
       rotation = 1,
-      rotationSize = 0,
       currentPos = [],
       position = (5, 20),
       pile = initialPile,
-      pause = True,
-      down = False
+      pause = False,
+      down = False,
+      start = False
     }
            
-init = (model, generate RandomFig (int 1 7))
+init = (model, Cmd.none)
        
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch [downs Key,
-               if model.pause
+               if model.pause || not model.start
                then Sub.none
                else if model.down
                     then every (10 * millisecond) Tick
@@ -47,25 +47,38 @@ subscriptions model =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Tick _ -> updateOnTick model
+        Tick _ -> case upd { model | position = (Tuple.first model.position,
+                                                     Tuple.second model.position - 1) } of
+                      Just newModel -> (newModel, Cmd.none)
+                      Nothing -> ({ model | pile =
+                                       destroyRow (model.currentPos ++ model.pile) 1,
+                                       position = (5, 20),
+                                       down = if model.down then False else model.down } ,
+                                 generate RandomFig (int 1 7))
                   
-        RandomFig rand -> let fig = get rand randomFigure
-                          in case Maybe.map size fig of
-                                 Just n -> ({ model | figure = fig,
-                                                  rotationSize = n },
-                                                generate RandomRot (int 0 (n - 1)))
-                                 Nothing -> (model, Cmd.none)
+        RandomFig rand -> case get rand randomFigure of
+                              Just fig -> ({ model | figure = fig },
+                                               generate RandomRot (int 0 (size fig - 1)))
+                              Nothing -> (model, Cmd.none)
 
-        RandomRot rot -> updateRotation model rot
+        RandomRot rot -> (Maybe.withDefault model (upd { model | rotation = rot }), Cmd.none)
 
         Key code -> case code of
                         32 -> ({ model | down = not model.down }, Cmd.none)
                         80 -> ({ model | pause = not model.pause }, Cmd.none)
-                        82 -> updateRotation model ((model.rotation + 1) % model.rotationSize)
-                        37 -> move model ((-) 1)
-                        39 -> move model ((+) 1)
+                        82 -> (Maybe.withDefault model (upd { model | rotation = 
+                                                           (model.rotation + 1) % (size model.figure) }) , Cmd.none)
+                        37 -> (Maybe.withDefault model (upd ({ model | position =
+                                                      (Tuple.first model.position - 1,
+                                                           Tuple.second model.position) })) ,
+                                   Cmd.none)
+                        39 -> (Maybe.withDefault model (upd ({ model | position =
+                                                      (Tuple.first model.position + 1,
+                                                           Tuple.second model.position) })) ,
+                                   Cmd.none)
+                        83 -> ({ model | start = not model.start }, generate RandomFig (int 1 7))
                         _ -> (model, Cmd.none)
-                            
+
 view : Model -> Html msg
 view { currentPos, pile } =
     div []
--- Update.elm
+++ Update.elm
@@ -1,41 +1,19 @@
-module Update exposing (..)
+module Update exposing (upd, destroyRow)
 
 import Random exposing (generate, int)
 import Types exposing (..)
 import Dict exposing (get)
 
-updateOnTick model =
-    let newCurrentPos = List.map (\(c, r) -> (c, r - 1)) model.currentPos
-    in if notPile model.pile newCurrentPos
-       then ({ model | currentPos = newCurrentPos,
-                                    position = (Tuple.first model.position, 
-                                                Tuple.second model.position - 1) }, Cmd.none)
-       else ({ model | pile = destroyRow (model.currentPos ++ model.pile) 1,
-                              position = (5, 20),
-                              down = if model.down then False else model.down },
-            generate RandomFig (int 1 7))
-
-updateRotation model rot =
-    let chooseRotation = model.figure |> Maybe.andThen (get rot)
-    in (changePosition chooseRotation model.position model rot, Cmd.none)
-                                                
-move model f =
-    let position = (abs (f (Tuple.first model.position)),
-                        Tuple.second model.position)
-        chooseRotation = model.figure |> Maybe.andThen (get model.rotation)
-    in (changePosition chooseRotation position model model.rotation, Cmd.none)
-               
-changePosition currentRot position model rotai =
-    case currentRot of
-        Just rot -> let (col, row) = position
-                        newCurrentPos = List.map (\(c, r) -> (col + c, row + r)) rot
-                    in if notPile model.pile newCurrentPos
-                       then { model | currentPos = newCurrentPos,
-                                  position = position,
-                                  rotation = rotai }
-                       else model
-        Nothing -> model
-
+upd model =
+    let chooseRotation = get model.rotation model.figure
+        (col, row) = model.position
+    in case chooseRotation of
+           Just rot -> let newPos = List.map (\(c, r) -> (col + c, row + r)) rot
+                       in if notPile model.pile newPos
+                          then Just { model | currentPos = newPos }
+                          else Nothing
+           Nothing -> Nothing
+                      
 notPile pile pos =
     List.all (\(c, r) -> List.all (\(co, ro) -> co /= c || ro /= r) pile) pos
 
